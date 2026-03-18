7caa13cbf607f523eeb9bde6156bf47978f9ae36
ilafper <ivanaalfaya@gmail.com>
2026-03-18 16:40:16 +0100

    terminado practicamente ajustar pequeños errores

--- api/app.js
+++ api/app.js
@@ -26,7 +26,7 @@ app.post("/api/crearclientsql", async (req, res) => {
   const code_user = 'codigo' + Math.floor(Math.random() * 1000);
   try {
     const sql = "INSERT INTO clientes (nombre, apellidos, telefono,direccion, correo, code_user) VALUES (?, ?, ?, ?,?,?)";
-    const [result] = await pool.execute(sql, [nombre, apellidos, telefono, correo, direccion, code_user]);
+    const [result] = await pool.execute(sql, [nombre, apellidos, telefono, direccion, correo, code_user]);
     res.json({ success: true, mensaje: "cliente creado correctamente"});
 
   } catch (error) {
@@ -44,7 +44,8 @@ app.delete("/api/deleteclientesql/:id_eliminar", async (req, res) => {
   console.log(id_eliminar);
   
   try {
-    const consulta = await pool.execute("DELETE FROM clientes WHERE code_user = ?", [id_eliminar]);
+    const [consulta] = await pool.execute("DELETE FROM clientes WHERE code_user = ?", [id_eliminar]);
+    
     if (consulta.affectedRows === 0) {
       return res.status(404).json({ success: false, error: "No se encontró el cliente" });
     }
@@ -54,4 +55,128 @@ app.delete("/api/deleteclientesql/:id_eliminar", async (req, res) => {
     res.status(500).json({ success: false, error: error.message });
   }
 });
+
+
+
+
+
+// filtro nombre sql 
+app.get("/api/filtronombresql/:nombreBusqueda", async (req, res) => {
+  
+ const filtroNombreSql = req.params.nombreBusqueda;
+ console.log(filtroNombreSql);
+ const filtroNuevo = "%"+ filtroNombreSql+ "%";
+ console.log(filtroNuevo);
+ 
+  try {
+    //select * from clientes where nombre like "%ari%";
+    const [consulta] = await pool.execute('select * from clientes where nombre like ?', [filtroNuevo] );
+    if (consulta.affectedRows === 0) {
+      return res.status(404).json({ success: false, error: "No se encontro ningun resultado" });
+    }
+
+    res.json({ success: true, mensaje:"todas las coincidencias", datos: consulta });
+
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Error al encontrar", detalle: error.message });
+  }
+});
+
+app.get("/api/filtroapellidossql/:apellidosBusqueda", async (req, res) => {
+  
+ const filtroApellidosql = req.params.apellidosBusqueda;
+ //console.log(filtroApellidosql);
+ const filtroNuevo = "%"+ filtroApellidosql+ "%";
+ //console.log(filtroNuevo);
+ 
+  try {
+    //select * from clientes where nombre like "%ari%";
+
+    // sin el corchete manda datos extra que no nos importan y lo ponemos en tre elos para solo el resultado de la busqueda
+    const [consulta] = await pool.execute('select * from clientes where apellidos like ?', [filtroNuevo] );
+    if (consulta.affectedRows === 0) {
+      return res.status(404).json({ success: false, error: "No se encontro ningun resultado" });
+    }
+
+    res.json({ success: true, mensaje:"todas las coincidencias", datos: consulta });
+
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Error al encontrar", detalle: error.message });
+  }
+});
+
+
+
+app.get("/api/filtrotelefono/:telefono", async (req, res) => {
+  
+ const filtroTelesql = req.params.telefono;
+ //console.log(filtroApellidosql);
+ const filtroNuevo = "%"+ filtroTelesql+ "%";
+ //console.log(filtroNuevo);
+ 
+  try {
+    //select * from clientes where nombre like "%ari%";
+
+    // sin el corchete manda datos extra que no nos importan y lo ponemos en tre elos para solo el resultado de la busqueda
+    const [consulta] = await pool.execute('select * from clientes where telefono like ?', [filtroNuevo] );
+    if (consulta.affectedRows === 0) {
+      return res.status(404).json({ success: false, error: "No se encontro ningun resultado" });
+    }
+
+    res.json({ success: true, mensaje:"todas las coincidencias", datos: consulta });
+
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Error al encontrar", detalle: error.message });
+  }
+});
+
+
+app.get("/api/filtrocorreo/:correo", async (req, res) => {
+  
+ const filtroCorreoSql = req.params.correo;
+ //console.log(filtroApellidosql);
+ const filtroNuevo = "%"+ filtroCorreoSql+ "%";
+ //console.log(filtroNuevo);
+ 
+  try {
+    //select * from clientes where nombre like "%ari%";
+
+    // sin el corchete manda datos extra que no nos importan y lo ponemos en tre elos para solo el resultado de la busqueda
+    const [consulta] = await pool.execute('select * from clientes where correo like ?', [filtroNuevo] );
+    if (consulta.affectedRows === 0) {
+      return res.status(404).json({ success: false, error: "No se encontro ningun resultado" });
+    }
+
+    res.json({ success: true, mensaje:"todas las coincidencias", datos: consulta });
+
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Error al encontrar", detalle: error.message });
+  }
+});
+
+app.get("/api/filtrodireccion/:direccion", async (req, res) => {
+  
+ const filtroDrireccionSql = req.params.direccion;
+ //console.log(filtroApellidosql);
+ const filtroNuevo = "%"+ filtroDrireccionSql+ "%";
+ //console.log(filtroNuevo);
+ 
+  try {
+    //select * from clientes where nombre like "%ari%";
+
+    // sin el corchete manda datos extra que no nos importan y lo ponemos en tre elos para solo el resultado de la busqueda
+    const [consulta] = await pool.execute('select * from clientes where direccion like ?', [filtroNuevo] );
+    if (consulta.affectedRows === 0) {
+      return res.status(404).json({ success: false, error: "No se encontro ningun resultado" });
+    }
+
+    res.json({ success: true, mensaje:"todas las coincidencias", datos: consulta });
+
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Error al encontrar", detalle: error.message });
+  }
+});
+
+
+
 module.exports = app;
